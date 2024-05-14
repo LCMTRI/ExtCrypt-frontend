210aa28b5685a533eabcb3a2faba0dc8ad250336
Le Cao Minh Tri <103999209+LCMTRI@users.noreply.github.com>
2024-05-14 15:53:45 +0700

    config for deployment

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,19 +1,17 @@
 import { DashboardNav } from "@/components/dashboard-nav";
-import { navItems } from "@/constants/data";
+import { NavLinks } from "@/constants/links";
 import { cn } from "@/lib/utils";
 
 export default function Sidebar() {
   return (
-    <nav
-      className={cn(`relative hidden h-screen border-r lg:block  w-72`)}
-    >
+    <nav className={cn(`relative hidden h-screen border-r lg:block  w-72`)}>
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <div className="space-y-1">
             <h2 className="mb-2 px-4 text-xl font-semibold tracking-tight">
               Overview
             </h2>
-            <DashboardNav items={navItems} />
+            <DashboardNav items={NavLinks} />
           </div>
         </div>
       </div>
