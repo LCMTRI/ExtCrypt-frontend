33c67e356b84edcf4f1598587ce6348697433ebf
Le Cao Minh Tri <103999209+LCMTRI@users.noreply.github.com>
2024-05-15 23:36:45 +0700

    config for deployment

--- components/google-auth-button.tsx
+++ components/google-auth-button.tsx
@@ -7,56 +7,12 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GoogleSignInButton() {
-  const router = useRouter();
-  const popupCenter = (url: string, title: string) => {
-    const dualScreenLeft = window.screenLeft ?? window.screenX;
-    const dualScreenTop = window.screenTop ?? window.screenY;
-
-    const width =
-      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
-
-    const height =
-      window.innerHeight ??
-      document.documentElement.clientHeight ??
-      screen.height;
-
-    const systemZoom = width / window.screen.availWidth;
-
-    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
-    const top = (height - 500) / 2 / systemZoom + dualScreenTop;
-
-    const newWindow = window.open(
-      url,
-      title,
-      `width=${500 / systemZoom},height=${
-        550 / systemZoom
-      },top=${top},left=${left}`,
-    );
-
-    newWindow?.focus();
-  };
-
-  useEffect(() => {
-    const handleMessage = (event: any) => {
-      if (event.data === "authComplete") {
-        router.push("/");
-      }
-    };
-
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, [router]);
-
   return (
     <Button
       className="w-full h-fit py-3"
       variant="outline"
       type="button"
-      // onClick={() => popupCenter("/signin/google-signin", "Google Signin")}
-      onClick={() => signIn("google")}
+      onClick={() => signIn("google", { callbackUrl: "/" })}
     >
       <Icons.google className="mr-2 h-5 w-5" />
       Continue with Google
