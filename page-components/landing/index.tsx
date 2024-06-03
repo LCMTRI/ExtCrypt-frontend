1753f81fce6116998b35d7d248e258c1957122b1
Le Cao Minh Tri <103999209+LCMTRI@users.noreply.github.com>
2024-06-03 20:19:26 +0700

    feat: update landing page

--- page-components/landing/index.tsx
+++ page-components/landing/index.tsx
@@ -11,7 +11,7 @@ const LandingPageComponent = () => {
       <HeroSection />
       <ReasonSection />
       <SampleSection />
-      <GuideSection />
+      {/* <GuideSection /> */}
     </div>
   );
 };
